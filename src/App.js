a36eecbeff5c849fb6cc1e358630dad6e8fec517
Giwan Persaud <giwan@knowble.nl>
2018-08-13 21:49:05 +0200

    removes unused boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Helmet from "react-helmet";
 
@@ -9,12 +8,8 @@ class App extends Component {
       <div className="App">
         <Helmet title="Woordmaatje Image galery" />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">WoordMaatje images</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
