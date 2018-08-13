df70d8c5359572a5a8f5097b92550e286051d8d6
Giwan Persaud <giwan@knowble.nl>
2018-08-13 21:33:11 +0200

    adds components for snapshots and helmets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Helmet from "react-helmet";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet title="Woordmaatje Image galery" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
